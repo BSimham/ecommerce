4c7bc94755f4d1daecb4fe7b1013b0386e0690fc
bharath <bsimhareddyls1@gmail.com>
2021-07-23 01:34:23 +0530

    calculated total values in cart.html

--- store/views.py
+++ store/views.py
@@ -17,8 +17,9 @@ def cart(request):
         items=order.orderitem_set.all() # it will get all the orderitems that have this order as parent
     else:
         items=[]
+        order={'get_cart_total':0, 'get_cart_items':0}
 
-    context={'items':items}
+    context={'items':items, 'order':order}
     return render(request,'store/cart.html',context)
 
 def checkout(request):
