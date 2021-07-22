3241312bb8b3c77f5f42cfe674dc7550866ea70b
bharath <bsimhareddyls1@gmail.com>
2021-07-22 13:02:05 +0530

    initialize views

--- store/views.py
+++ store/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 def store(request):
     context={}
     render(request,'store/store.html',context)
+def cart(request):
+    context={}
+    render(request,'store/cart.html',context)
+def checkout(request):
+    context={}
+    render(request,'store/checkout.html',context)
