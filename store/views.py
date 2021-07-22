eb365e668edab3069e1bc0ee96331ef6829167b2
bharath <bsimhareddyls1@gmail.com>
2021-07-23 01:43:13 +0530

    made checkout.html dynamic

--- store/views.py
+++ store/views.py
@@ -23,5 +23,14 @@ def cart(request):
     return render(request,'store/cart.html',context)
 
 def checkout(request):
-    context={}
+
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created=Order.objects.get_or_create(customer,complete=False) #creating an object or quering one
+        items=order.orderitem_set.all() # it will get all the orderitems that have this order as parent
+    else:
+        items=[]
+        order={'get_cart_total':0, 'get_cart_items':0}
+    
+    context={'items':items, 'order':order}
     return render(request,'store/checkout.html',context)
