33ec39cbee005c19b3ea6b296399ba1b4a1e4d06
bharath <bsimhareddyls1@gmail.com>
2021-07-23 00:43:19 +0530

    modified product class and migrated it. set media url to media dir .

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 import debug_toolbar
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
     path('__debug__/',include(debug_toolbar.urls)),
 ]
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
