79102e2957cae6b55e61ef36b08c43139cb66133
bharath <bsimhareddyls1@gmail.com>
2021-07-23 01:19:42 +0530

    making cart dynamic

--- store/views.py
+++ store/views.py
@@ -2,13 +2,25 @@ from store.models import Product
 from django.shortcuts import render
 from .models import *
 # Create your views here.
+
+
 def store(request):
     products=Product.objects.all()
     context={'products':products}
     return render(request,'store/store.html',context)
+
 def cart(request):
-    context={}
+
+    if request.user.is_authenticated:
+        customer=request.user.customer
+        order,created=Order.objects.get_or_create(customer,complete=False) #creating an object or quering one
+        items=order.orderitem_set.all() # it will get all the orderitems that have this order as parent
+    else:
+        items=[]
+
+    context={'items':items}
     return render(request,'store/cart.html',context)
+
 def checkout(request):
     context={}
     return render(request,'store/checkout.html',context)
