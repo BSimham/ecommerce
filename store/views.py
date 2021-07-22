bad18831cccc76b4df7b690f115597105677727c
bharath <bsimhareddyls1@gmail.com>
2021-07-22 22:08:58 +0530

    setting up main.css and pages store.html , main.html, cart.html

--- store/views.py
+++ store/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def store(request):
     context={}
-    render(request,'store/store.html',context)
+    return render(request,'store/store.html',context)
 def cart(request):
     context={}
-    render(request,'store/cart.html',context)
+    return render(request,'store/cart.html',context)
 def checkout(request):
     context={}
-    render(request,'store/checkout.html',context)
+    return render(request,'store/checkout.html',context)
